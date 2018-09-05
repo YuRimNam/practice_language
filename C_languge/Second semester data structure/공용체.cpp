97710fae35784fbaf4aefed4762f1b1cc29f89b6
milkyway002 <sea_bom_12@naver.com>
2018-09-05 11:28:41 +0900

    update new union code

--- C_languge/Second semester data structure/공용체.cpp
+++ C_languge/Second semester data structure/공용체.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-union student {
+/*union student {
 	int id;
 	int no;
 } s1;
@@ -10,4 +10,14 @@ int main() {
 	printf("%d %d\n", s1.id, s1.no);
 	s1.no = 17;
 	printf("%d %d", s1.id, s1.no);
+}*/
+
+union ip_addr {
+	unsigned int addr;
+	unsigned char saddr[4];
+} ip;
+
+int main() {
+	ip.addr=0x01020304;
+	printf("%d.%d.%d.%d", ip.saddr[3],ip.saddr[2],ip.saddr[1],ip.saddr[0]); 
 }
